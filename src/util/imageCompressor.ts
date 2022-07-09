74cf5b7669b9f8c455e9ceff335f392fd5b46338
Italo Holanda <italo.holanda@escolaconquer.com.br>
2022-07-09 20:48:48 -0300

    feature: added image compressor function

---
+++ src/util/imageCompressor.ts
@@ -0,0 +1,33 @@
+import imageCompression from 'browser-image-compression'
+
+const ONE_MEGABYTE = 1048576
+
+function isCompressed(imageFile: File, maxSizeMB: number) {
+  return imageFile.size <= ONE_MEGABYTE * maxSizeMB
+}
+
+export default async function imageCompressor(
+  imageFile: File,
+  maxSizeMB = 1.5
+) {
+  const controller = new AbortController()
+
+  if (isCompressed(imageFile, maxSizeMB)) return imageFile
+
+  const options = {
+    maxSizeMB: maxSizeMB,
+    signal: controller.signal
+  }
+
+  setTimeout(function () {
+    controller.abort(new Error('Image compressor error: timed out'))
+  }, 60000)
+
+  try {
+    const compressedImage = await imageCompression(imageFile, options)
+    return compressedImage
+  } catch (error) {
+    console.log(error)
+    throw new Error('Failed to compress image file')
+  }
+}
