a4f929fca2cd5226007135d66305b16ec951214f
Italo Holanda <italo.holanda@escolaconquer.com.br>
2022-07-09 17:01:08 -0300

    refactor: improved color system

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,5 +1,8 @@
 import { createGlobalStyle } from 'styled-components'
 
+export const typoSystem =
+  "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif"
+
 const GlobalStyles = createGlobalStyle`
   * {
     margin: 0;
@@ -15,6 +18,7 @@ const GlobalStyles = createGlobalStyle`
     --grey-dark: #1E1E1E;
     --grey-lighten: #404040;
     --grey-light: #D9D9D9;
+    --grey-medium: #1A1A1A;
   }
 
   html, body, #__next {
@@ -22,9 +26,14 @@ const GlobalStyles = createGlobalStyle`
   }
 
   body {
-    font-family: "IBM Plex Sans", -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family: "IBM Plex Sans", ${typoSystem};
     background: var(--grey-dark);
     color: var(--white);
+  }
+
+  h1 {
+    font-size: 1.3rem;
+    font-family: "IBM Plex Mono", ${typoSystem};
   }
 `
 
